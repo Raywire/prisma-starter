f6db11a84c69b79543bad2192c18cdf799110c6d
Ryan Wire <ryanwire@outlook.com>
2020-12-19 10:55:32 +0300

    Writes data into the database

--- script.ts
+++ script.ts
@@ -4,10 +4,19 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
+  const post = await prisma.post.create({
+    data: {
+      title: "Prisma makes databases easy",
+      author: {
+        connect: { email: "sarah@prisma.io" },
+      },
+    },
+  })
+  console.log(post)
+
   const allUsers = await prisma.user.findMany({
     include: { posts: true },
   })
-  // use `console.dir` to print nested objects
   console.dir(allUsers, { depth: null })
 }
 
